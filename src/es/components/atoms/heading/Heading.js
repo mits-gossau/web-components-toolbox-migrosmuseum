595ba4b61ada4abe485477365904af174d56d2c0
Weedshaker <weedshaker@gmail.com>
2025-11-24 18:07:37 +0100

    shadow fixes sub

--- src/es/components/atoms/heading/Heading.js
+++ src/es/components/atoms/heading/Heading.js
@@ -1,5 +1,22 @@
 // @ts-check
 import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+import { Intersection } from '../../web-components-toolbox/src/es/components/prototypes/Intersection.js'
+
+// Sensor used when attribute is sticky
+class IntersectionSensor extends Intersection() {
+  constructor (intersectionFunction, options = {}, ...args) {
+    super({
+      intersectionObserverInit: {},
+      tabindex: 'no-tabindex-style',
+      ...options
+    }, ...args)
+
+    this.intersectionFunction = intersectionFunction
+  }
+  intersectionCallback (entries, observer) {
+    this.intersectionFunction(this.areEntriesIntersecting(entries))
+  }
+}
 
 /**
 * @export
@@ -16,7 +33,10 @@ export default class Heading extends Shadow() {
     const showPromises = []
     if (this.shouldRenderCSS()) showPromises.push(this.renderCSS())
     if (this.shouldRenderHTML()) showPromises.push(this.renderHTML())
-    if (this.hasAttribute('picture-load')) showPromises.push(/** @type {Promise<void>} */(new Promise(resolve => this.nextElementSibling.addEventListener('picture-load', event => resolve(), { once: true }))))
+    const nextElementSibling = this.hasAttribute('sticky')
+      ? this.nextElementSibling?.nextElementSibling || this.nextElementSibling
+      : this.nextElementSibling
+    if (this.hasAttribute('picture-load')) showPromises.push(/** @type {Promise<void>} */(new Promise(resolve => nextElementSibling.addEventListener('picture-load', event => resolve(), { once: true }))))
     Promise.all(showPromises).then(() => {
       // fix z-index covering this heading to a part (height)
       const fixZIndex = node => {
@@ -28,18 +48,31 @@ export default class Heading extends Shadow() {
         `
         return isWebComponent
       }
-      if (this.hasAttribute('shadow') && !fixZIndex(this.nextElementSibling)) setTimeout(() => fixZIndex(this.nextElementSibling), 50)
+      if (this.hasAttribute('shadow') && !fixZIndex(nextElementSibling)) setTimeout(() => fixZIndex(nextElementSibling), 50)
       // set box-shadow late for slow transition, looks better with elements below building up
-      this.css = /* css */`
-        :host([shadow]) {
-          box-shadow: var(--box-shadow);
-        }
-        @media only screen and (max-width: _max-width_) {
-          :host([shadow]) {
-            box-shadow: var(--box-shadow-mobile);
+      if (this.hasAttribute('sticky') && typeof nextElementSibling.setCss === 'function') {
+        nextElementSibling.setCss(/* css */`
+          :host > section {
+            box-shadow: var(--box-shadow);
           }
-        }
-      `
+          @media only screen and (max-width: _max-width_) {
+            :host > section {
+              box-shadow: var(--box-shadow-mobile);
+            }
+          }
+        `, undefined, false)
+      } else {
+        this.css = /* css */`
+          :host([shadow]) {
+            box-shadow: var(--box-shadow-inset);
+          }
+          @media only screen and (max-width: _max-width_) {
+            :host([shadow]) {
+              box-shadow: var(--box-shadow-mobile-inset);
+            }
+          }
+        `
+      }
       this.hidden = false
     })
   }
@@ -70,9 +103,17 @@ export default class Heading extends Shadow() {
   renderCSS () {
     this.css = /* css */`
       :host {
+        display: flex !important;
         width: 100% !important;
         padding: 0 var(--content-spacing, unset);
         transition: box-shadow 3s ease-in;
+      }
+      :host([sticky]) {
+        position: sticky;
+        top: 0;
+      }
+      :host([hidden]) {
+        display: none !important;
       }
       :host > * {
         margin: 0 !important;
@@ -80,7 +121,7 @@ export default class Heading extends Shadow() {
       :host([crop]) > * {
         transform: translateY(0.35em);
         margin-top: -0.35em !important;
-        clip-path: rect(0 100% calc(100% - 0.35em) 0)
+        clip-path: rect(0 100% calc(100% - 0.35em) 0);
       }
       :host([fix-first-letter-spacing]) > * {
         transform: translateX(-0.075em);
@@ -132,5 +173,20 @@ export default class Heading extends Shadow() {
     // fix font letter-spacing manually, since css does not yet support extended font letter-spacing
     if (['b', 'B', 'D', 'E', 'F', 'h', 'H', 'i', 'I', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'p', 'P', 'r', 'R', 'u', 'U'].includes(this.heading.textContent.substring(0, 1))) this.setAttribute('fix-first-letter-spacing', '')
     this.html = this.heading
+    // make a hidden element to track if this component would still be visible after scrolling (sticky does not scroll, so we don't know where we would be scrolled, if this components was static)
+    if (this.hasAttribute('sticky')) {
+      // @ts-ignore
+      customElements.define('heading-intersection-sensor', IntersectionSensor)
+      const intersectionSensor = new IntersectionSensor(isIntersecting => {
+        if (isIntersecting) {
+          this.removeAttribute('hidden')
+        } else {
+          this.setAttribute('hidden', '')
+        }
+      })
+      intersectionSensor.setAttribute('style', 'visibility: hidden; width: 0; height: 0; padding: 0; margin: 0;')
+      this.insertAdjacentElement('afterend', intersectionSensor)
+    }
+    return Promise.resolve()
   }
 }
