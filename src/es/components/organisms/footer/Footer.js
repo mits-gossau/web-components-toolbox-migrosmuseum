83f8eb6ce2ff1b5bcb424dc557c07f2170b16870
Weedshaker <weedshaker@gmail.com>
2025-12-24 11:30:03 +0100

    footer sub

---
+++ src/es/components/organisms/footer/Footer.js
@@ -0,0 +1,96 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+
+/**
+* @export
+* @class Footer
+* @type {CustomElementConstructor}
+*/
+export default class Footer extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, tabindex: 'no-tabindex', ...options }, ...args)
+  }
+
+  connectedCallback () {
+    this.hidden = true
+    const showPromises = []
+    if (this.shouldRenderCSS()) showPromises.push(this.renderCSS())
+    if (this.shouldRenderHTML()) showPromises.push(this.renderHTML())
+    Promise.all(showPromises).then(() => (this.hidden = false))
+  }
+
+  disconnectedCallback () {}
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(`${this.cssSelector} > style[_css]`)
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderHTML () {
+    return !this.div
+  }
+
+  /**
+   * renders the css
+   * @returns Promise<void>
+   */
+  renderCSS () {
+    this.css = /* css */`
+      :host {
+        grid-area: footer;
+      }
+      @media only screen and (max-width: _max-width_) {
+        :host {}
+      }
+    `
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   */
+  fetchTemplate () {
+    /** @type {import("../../web-components-toolbox/src/es/components/prototypes/Shadow.js").fetchCSSParams[]} */
+    const styles = [
+      {
+        path: `${this.importMetaUrl}../../web-components-toolbox/src/css/reset.css`, // no variables for this reason no namespace
+        namespace: false
+      },
+      {
+        path: `${this.importMetaUrl}../../web-components-toolbox/src/css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+        namespaceFallback: true
+      }
+    ]
+    switch (this.getAttribute('namespace')) {
+      case 'footer-default-':
+        return this.fetchCSS([{
+          path: `${this.importMetaUrl}./default-/default-.css`, // apply namespace since it is specific and no fallback
+          namespace: false
+        }, ...styles], false) // using showPromises @connectedCallback makes hide action inside Shadow.fetchCSS obsolete, so second argument hide = false
+      default:
+        return this.fetchCSS(styles, false)
+    }
+  }
+
+  /**
+   * Render HTML
+   * @returns Promise<void>
+   */
+  renderHTML () {
+    console.log('*********', 'render')
+    this.html = '<div>Content rendered from Component: Footer</div>'
+  }
+
+  get div () {
+    return this.root.querySelector('div')
+  }
+}
