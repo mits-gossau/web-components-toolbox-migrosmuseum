c3e75a216eb4b07a01fb19ea59cbe3128fe27154
Weedshaker <weedshaker@gmail.com>
2026-04-20 17:55:41 +0200

    sub animation fixes --- good by --- (╥﹏╥) ---

--- src/es/components/molecules/navigation/Navigation.js
+++ src/es/components/molecules/navigation/Navigation.js
@@ -5,91 +5,78 @@ import Navigation from '../../web-components-toolbox/src/es/components/molecules
 /* global CustomEvent */
 
 /**
- * Navigation hosts uls
- * Example at: /src/es/components/pages/Home.html
- * As a molecule, this component shall hold Atoms
- *
- * @export
- * @class Navigation
- * @type {CustomElementConstructor}
- * @attribute {
- *  {boolean} [hover=false]
- *  {boolean} [hit-area=true] this lets you define a hit-area of your link, to avoid too large focus (hit-area) by fonts too large line-height, which can't be overwritten with css: https://github.com/mits-gossau/web-components-cms-template/issues/53
- *  {boolean} [focus-lost-close=false] tell it to close when focus is lost
- * }
- * @css {
- *  --content-spacing [40px]
- *  --a-link-content-spacing [0]
- *  --a-link-font-size [1rem]
- *  --background-color [black]
- *  --list-style [none]
- *  --align-items [start]
- *  --min-width [100px] of list items at second level
- *  --padding-top [6px] first list item at second level
- *  --hr-color [white]
- *  --a-link-font-size-mobile [2rem]
- *  --font-weight-mobile [normal]
- *  --a-link-text-align-mobile [center]
- *  --justify-content-mobile [center]
- *  --a-arrow-color-hover [--color-hover, white]
- *  --a-arrow-color [#777]
- *  --min-height-mobile [50px]
- *  --min-width-mobile [50px]
- * }
+ * MigrosmuseumNavigation is mainly for the li hover up animation like file drawer
  */
 export default class MigrosmuseumNavigation extends Navigation {
   constructor (options = {}, ...args) {
     super(options, ...args)
 
+    this.translateY = '1em'
+
+    // timeout because the mouse-over reacts on each li element hover separately even though we listen on the ul
+    let mouseoverListenerTimeoutId = null
     let mouseoutListenerTimeoutId = null
     this.mouseoverListener = event => {
+      clearTimeout(mouseoverListenerTimeoutId)
       clearTimeout(mouseoutListenerTimeoutId)
-      if (!this.hasAttribute('mouse-over')) {
-        this.customStyle.textContent = Array.from(this.root.querySelectorAll('nav > ul:first-of-type > li')).reduce((acc, curr, i) => {
-          const currBoundingClientRect = curr.getBoundingClientRect()
-          return /* css */`
-            ${acc}
-            :host > nav > ul:first-of-type > li:nth-child(${i + 1}) {
-              bottom: ${(self.innerHeight - currBoundingClientRect.bottom).toFixed(2)}px;
-              left: ${currBoundingClientRect.left.toFixed(2)}px;
-            }
-          `
-        }, '') + Array.from(this.root.querySelectorAll('nav > ul')).reduce((acc, curr, i) => {
-          const currBoundingClientRect = curr.getBoundingClientRect()
-          return /* css */`
-            ${acc}
-            :host > nav > ul:nth-child(${i + 1}) {
-              bottom: ${(self.innerHeight - currBoundingClientRect.bottom).toFixed(2)}px;
-              left: ${currBoundingClientRect.left.toFixed(2)}px;
-            }
-          `
-        }, '')
-        this.setAttribute('mouse-over', '')
-        console.log('***mouseoverListener******', event, this.customStyle.textContent)
-      }
+      mouseoverListenerTimeoutId = setTimeout(() => {
+        if (!this.hasAttribute('mouse-over') && !this.hasAttribute('mouse-over-dialog-opening-closing')) {
+          this.setCustomStyle() // set all li elements position absolute with current coordinates to make them static for hover animation
+          this.setAttribute('mouse-over', '')
+        }
+      }, 50)
     }
 
     this.mouseoutListener = event => {
       clearTimeout(mouseoutListenerTimeoutId)
+      clearTimeout(mouseoverListenerTimeoutId)
       mouseoutListenerTimeoutId = setTimeout(() => {
-        console.log('***mouseoutListener******', event)
         this.customStyle.textContent = ''
         this.removeAttribute('mouse-over')
       }, 50)
+    }
+
+    let animationendListener
+    this.dialogOpenCloseListener = event => {
+      const hasAttributeMouseOver = this.hasAttribute('mouse-over')
+      if (hasAttributeMouseOver) {
+        this.setAttribute('mouse-over-dialog-opening-closing', '')
+        this.removeAttribute('mouse-over')
+      }
+      this.customStyle.textContent = ''
+      if (animationendListener) this.removeEventListener('animationend', animationendListener)
+      this.addEventListener('animationend', (animationendListener = event => {
+        let thisLi
+        if ((thisLi = event.composedPath().find(el => el.tagName === 'LI')) && thisLi.previousElementSibling) {
+          this.setCustomStyle({ adjust: this.translateY, target: thisLi.previousElementSibling })
+        } else {
+          this.setCustomStyle()
+        }
+        if (hasAttributeMouseOver) {
+          this.setAttribute('mouse-over', '')
+          this.removeAttribute('mouse-over-dialog-opening-closing')
+        }
+      }, {once: true}))
     }
   }
 
   connectedCallback () {
     super.connectedCallback()
-    this.root.querySelector('nav > ul').addEventListener('mouseover', this.mouseoverListener)
-    this.root.querySelector('nav > ul').addEventListener('mouseout', this.mouseoutListener)
+    const ul = this.root.querySelector('nav > ul')
+    ul.addEventListener('mouseover', this.mouseoverListener)
+    ul.addEventListener('mouseout', this.mouseoutListener)
+    this.addEventListener('open', this.dialogOpenCloseListener)
+    this.addEventListener('close', this.dialogOpenCloseListener)
     this.html = this.customStyle
   }
 
   disconnectedCallback () {
     super.disconnectedCallback()
-    this.root.querySelector('nav > ul').removeEventListener('mouseover', this.mouseoverListener)
-    this.root.querySelector('nav > ul').removeEventListener('mouseout', this.mouseoutListener)
+    const ul = this.root.querySelector('nav > ul')
+    ul.removeEventListener('mouseover', this.mouseoverListener)
+    ul.removeEventListener('mouseout', this.mouseoutListener)
+    this.removeEventListener('open', this.dialogOpenCloseListener)
+    this.removeEventListener('close', this.dialogOpenCloseListener)
   }
 
   /**
@@ -113,20 +100,48 @@ export default class MigrosmuseumNavigation extends Navigation {
       :host([mouse-over]) nav > ul:first-of-type > li {
         position: absolute;
       }
-      :host([mouse-over]) nav > ul:first-of-type > li, :host([mouse-over]) nav > ul:first-of-type > li > *::part(summary) {
+      :host nav > ul:first-of-type > li,
+      :host nav > ul:first-of-type > li > *::part(summary),
+      :host nav > ul:first-of-type > li > *::part(content),
+      :host nav > ul:first-of-type > li > *::part(content-child) {
         transition: transform 0.25s ease-in-out;
       }
-      :host([mouse-over]) nav > ul:first-of-type > li:has(+ li:hover), :host([mouse-over]) nav > ul:first-of-type > li:hover > *::part(summary) {
-        transform: translateY(-1em);
+      :host(:where([mouse-over], [mouse-over-dialog-opening-closing])) nav > ul:first-of-type > li:has(+ li:hover),
+      :host(:where([mouse-over], [mouse-over-dialog-opening-closing])) nav > ul:first-of-type > li:hover > *::part(summary) {
+        transform: translateY(-${this.translateY});
       }
-      :host([mouse-over]) nav > ul:first-of-type > li > *::part(summary) {
-        transition: transform 0.25s ease-in-out;
+      :host(:where([mouse-over], [mouse-over-dialog-opening-closing])) nav > ul:first-of-type > li:has(+ li:hover) > *::part(summary),
+      :host(:where([mouse-over], [mouse-over-dialog-opening-closing])) nav > ul:first-of-type > li:has(+ li:hover) > *::part(content) {
+        transform: translateY(${this.translateY});
       }
-      :host([mouse-over]) nav > ul:first-of-type > li:has(+ li:hover) > *::part(summary) {
-        transform: translateY(1em);
+      :host(:where([mouse-over], [mouse-over-dialog-opening-closing])) nav > ul:first-of-type > li:hover > *::part(content-child):hover {
+        transform: translateY(-0.15em);
       }
     `
     return result
+  }
+
+  setCustomStyle (options = {}) {
+    const { adjust, target } = options
+    this.customStyle.textContent = Array.from(this.root.querySelectorAll('nav > ul:first-of-type > li')).reduce((acc, curr, i) => {
+      const currBoundingClientRect = curr.getBoundingClientRect()
+      return /* css */`
+        ${acc}
+        :host > nav > ul:first-of-type > li:nth-child(${i + 1}) {
+          bottom: calc(${(self.innerHeight - currBoundingClientRect.bottom).toFixed(2)}px - ${curr === target ? adjust || '0px' : '0px'});
+          left: 0;
+        }
+      `
+    }, '') + Array.from(this.root.querySelectorAll('nav > ul')).reduce((acc, curr, i) => {
+      const currBoundingClientRect = curr.getBoundingClientRect()
+      return /* css */`
+        ${acc}
+        :host > nav > ul:nth-child(${i + 1}) {
+          bottom: ${(self.innerHeight - currBoundingClientRect.bottom).toFixed(2)}px;
+          left: 0;
+        }
+      `
+    }, '')
   }
 
   get customStyle () {
