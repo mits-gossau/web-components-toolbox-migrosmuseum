ad27c80bde921cfdfff4a7c3e91558ca86385a67
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2026-08-06 09:27:55 +0200

    test(footer): align mobile logo expectation [NO-TICKET]

--- test/footer-font-size.spec.js
+++ test/footer-font-size.spec.js
@@ -5,7 +5,7 @@ const { test, expect } = require('@playwright/test')
 const localSite = process.env.UMBRACO_BASE_URL
 const pagePath = '/programm/ausstellungen/disobedience-archive-canopy-for-broken-time'
 const viewports = [
-  { name: 'mobile', width: 390, height: 844, sectionFontSize: '13px', textFontSize: '13px', linkLineHeight: '17px', logoWidth: '150px' },
+  { name: 'mobile', width: 390, height: 844, sectionFontSize: '13px', textFontSize: '13px', linkLineHeight: '17px', logoWidth: '136px' },
   { name: 'desktop', width: 1440, height: 900, sectionFontSize: '23px', textFontSize: '19px', linkLineHeight: '21px', logoWidth: '184px' }
 ]
 
